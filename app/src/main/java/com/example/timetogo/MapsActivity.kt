069b67fce0c66bfcc544fbf376e305bcb3c59dfc
d.misic <d.misic@levi9.com>
2019-05-08 16:11:26 +0200

    My location setup

--- app/src/main/java/com/example/timetogo/MapsActivity.kt
+++ app/src/main/java/com/example/timetogo/MapsActivity.kt
@@ -1,7 +1,11 @@
 package com.example.timetogo
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import com.google.android.gms.location.GeofencingClient
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -10,20 +14,27 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import pub.devrel.easypermissions.AfterPermissionGranted
+import pub.devrel.easypermissions.EasyPermissions
 
-lateinit var geofencingClient: GeofencingClient
+private const val LOCATION_PERMISSION_REQUEST_CODE = 1
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMyLocationClickListener, GoogleMap.OnMyLocationButtonClickListener, EasyPermissions.PermissionCallbacks  {
 
 
-class MainActivity : AppCompatActivity(), OnMapReadyCallback  {
-
+    private var showPermissionDeniedDialog: Boolean  = false
     val SYDNEY = LatLng(-33.862, 151.21)
     val ZOOM_LEVEL = 13f
+    lateinit var geofencingClient: GeofencingClient
+    lateinit var map: GoogleMap
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         geofencingClient = LocationServices.getGeofencingClient(this)
+
+
         val mapFragment : SupportMapFragment? =
             supportFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
         mapFragment?.getMapAsync(this)
@@ -32,9 +43,72 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback  {
 
     override fun onMapReady(googleMap: GoogleMap?) {
         googleMap ?: return
-        with(googleMap) {
+
+        map = googleMap
+
+        enableMyLocation()
+        /*with(map) {
             moveCamera(CameraUpdateFactory.newLatLngZoom(SYDNEY, ZOOM_LEVEL))
             addMarker(MarkerOptions().position(SYDNEY))
+        }*/
+
+        map.setOnMyLocationButtonClickListener(this)
+        map.setOnMyLocationClickListener(this)
+    }
+
+    @SuppressLint("MissingPermission")
+    @AfterPermissionGranted(LOCATION_PERMISSION_REQUEST_CODE)
+    private fun enableMyLocation() {
+        // Enable the location layer. Request the location permission if needed.
+        val permissions = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION)
+
+        if (EasyPermissions.hasPermissions(this, *permissions)) {
+            map.isMyLocationEnabled = true
+
+        } else {
+            // if permissions are not currently granted, request permissions
+            EasyPermissions.requestPermissions(this,
+                getString(R.string.permission_rationale_location),
+                LOCATION_PERMISSION_REQUEST_CODE, *permissions)
         }
+    }
+
+    /**
+     * Display a dialog box asking the user to grant permissions if they were denied
+     */
+    override fun onResumeFragments() {
+        super.onResumeFragments()
+        if (showPermissionDeniedDialog) {
+            AlertDialog.Builder(this).apply {
+                setPositiveButton(R.string.ok, null)
+                setMessage(R.string.location_permission_denied)
+                create()
+            }.show()
+            showPermissionDeniedDialog = false
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode,
+            permissions, grantResults, this)
+    }
+
+    override fun onMyLocationClick(p0: Location) {
+    }
+
+    override fun onMyLocationButtonClick(): Boolean {
+        return false
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+        showPermissionDeniedDialog = true
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
     }
 }
